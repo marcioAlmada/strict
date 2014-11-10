01441eb0fdc1a8a80a06aa16e2e81ff1ede6a20a
krakjoe <joe.watkins@live.co.uk>
2014-11-10 16:45:01 +0000

    ...

--- strict.c
+++ strict.c
@@ -134,7 +134,7 @@ static inline int php_strict_autobox_cast(zval *read, zval *write, int type TSRM
         break;
     }
     
-    zend_throw_exception_ex(ce_TypeException, IS_STRING TSRMLS_CC,
+    zend_throw_exception_ex(ce_TypeException, autobox->type TSRMLS_CC,
         "illegal cast to %s from %s", 
         zend_get_type_by_const(type), 
         zend_get_type_by_const(autobox->type));
